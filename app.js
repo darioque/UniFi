8c70df4c1c6ab2ff90163fd0801b7664f8990676
Darioque <darioskidelsky@gmail.com>
2021-11-07 17:17:50 -0300

    cambia el conversor a un formulario, añade .ds_store a .gitignore

--- app.js
+++ app.js
@@ -25,3 +25,10 @@ app.get("/product-detail", (req, res) => {
 app.get("/trade-confirmation", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/tradeConfirmation.html"));
 });
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
