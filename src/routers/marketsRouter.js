aeca8aa20b537aa5966398c207ea7e03c1304871
Darioque <darioskidelsky@gmail.com>
2021-11-18 21:35:52 -0300

    modificaciones durante clase

--- src/routers/marketsRouter.js
+++ src/routers/marketsRouter.js
@@ -6,5 +6,9 @@ router.get('/', marketsController.markets);
 router.get('/:marketType', marketsController.type);
 router.get('/:marketType/:asset', marketsController.detail);
 router.get('/:marketType/:asset/trade-confirmation', marketsController.tradeConfirmation);
+router.post(
+  "/:marketType/:asset/trade-confirmation",
+  marketsController.tradeConfirmation
+);
 
 module.exports = router;
