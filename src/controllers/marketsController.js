563aeb40eaae2ea684e6a832a9f4ff13294647cc
Darioque <darioskidelsky@gmail.com>
2021-11-21 03:39:41 -0300

    agrega pop-up de confirmacion a productDetail

--- src/controllers/marketsController.js
+++ src/controllers/marketsController.js
@@ -42,16 +42,17 @@ const marketsController = {
         });
     },
     type: function (req, res) {
-        res.render(String(req.params.marketType), {
-            pageTitle: "Invest in UniFi"
+        let marketType = req.params.marketType
+        res.render(String(marketType + 'List'), {
+            pageTitle: "Invest in UniFi - " + marketType,
+            assetList: assetList,
         });
     },
     detail: function (req, res) {
         let assetRequested = req.params.asset;
-        res.render(String('productDetail'), {
+        res.render(String('productDetail2'), {
             pageTitle: assetRequested + " Details",
-            assetList: assetList,
-            asset: assetList.find(asset => asset.name === assetRequested),
+            asset: assetList.find(asset => asset.name === assetRequested || asset.ticker.toLowerCase() === assetRequested),
         });
     },
     tradeConfirmation: function(req, res) {
