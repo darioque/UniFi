d00d5b960c07deec87d8743de6d22f8bf8bbadfb
fjloromeyer <fjloromeyer@gmail.com>
2021-11-21 20:00:24 -0300

    igualo estructura visual entre home y productDetail

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "../public");
 
-const mainRouter = require('./routers/mainRouter.js');
-const marketsRouter = require('./routers/marketsRouter.js')
+const mainRouter = require("./routers/mainRouter.js");
+const marketsRouter = require("./routers/marketsRouter.js");
 
 // seteamos ruta de archivos estaticos y view engine por defecto
 app.use(express.static(publicPath));
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // ejecutamos el servidor
 app.listen(process.env.PORT || 3000, () => {
@@ -16,12 +16,9 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 // rutas
-app.use('/', mainRouter);
-app.use('/markets', marketsRouter)
-
+app.use("/", mainRouter);
+app.use("/markets", marketsRouter);
 
 app.get("/trade-confirmation", (req, res) => {
   res.render("tradeConfirmation");
 });
-
-
