8c7b66960d388cba8799c2d056dcfa30d2be6912
LeonardoBujan <tatobujan86@gmail.com>
2021-11-22 14:35:49 -0300

    Creacion de archivo products.js y modificacion de marketsController.js

--- src/controllers/marketsController.js
+++ src/controllers/marketsController.js
@@ -1,40 +1,5 @@
-let assetList = [
-    {
-        name: 'bitcoin',
-        type: 'cryptocurrencies',
-        ticker: 'BTC',
-        price: '60,000',
-        logo: '/img/btc_logo.png' 
-    },
-    {
-        name: 'ethereum',
-        type: 'cryptocurrencies',
-        ticker: 'ETH',
-        price: '60,000',
-        logo: '/img/eth_logo.png' 
-    },
-    {
-        name: 'amazon',
-        type: 'stocks',
-        ticker: 'AMZN',
-        price: '60,000',
-        logo: '/img/amzn_logo.png' 
-    },
-    {
-        name: 'tesla',
-        type: 'stocks',
-        ticker: 'TSLA',
-        price: '60,000',
-        logo: '/img/tsla_logo.png' 
-    },
-    {
-        name: 'google',
-        type: 'stocks',
-        ticker: 'GOOGL',
-        price: '60,000',
-        logo: '/img/googl_logo.png'  
-    },
-];
+let assetList = require("./products");
+
 const marketsController = {
     markets: function (req, res) {
         res.render('markets', {
---
+++ src/controllers/products.js
@@ -0,0 +1,39 @@
+let listProduct = [
+    {
+        name: 'bitcoin',
+        type: 'cryptocurrencies',
+        ticker: 'BTC',
+        price: '60,000',
+        logo: '/img/btc_logo.png' 
+    },
+    {
+        name: 'ethereum',
+        type: 'cryptocurrencies',
+        ticker: 'ETH',
+        price: '60,000',
+        logo: '/img/eth_logo.png' 
+    },
+    {
+        name: 'amazon',
+        type: 'stocks',
+        ticker: 'AMZN',
+        price: '60,000',
+        logo: '/img/amzn_logo.png' 
+    },
+    {
+        name: 'tesla',
+        type: 'stocks',
+        ticker: 'TSLA',
+        price: '60,000',
+        logo: '/img/tsla_logo.png' 
+    },
+    {
+        name: 'google',
+        type: 'stocks',
+        ticker: 'GOOGL',
+        price: '60,000',
+        logo: '/img/googl_logo.png'  
+    },
+];
+
+module.exports = listProduct;
