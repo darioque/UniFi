84cfe21bb6ced6b95dbacbc4c04d8f7e04182bf9
Darioque <darioskidelsky@gmail.com>
2021-11-23 08:02:15 -0300

    fix marketsRouter

--- src/routers/marketsRouter.js
+++ src/routers/marketsRouter.js
@@ -5,7 +5,6 @@ const marketsController = require('../controllers/marketsController');
 router.get('/', marketsController.markets);
 router.get('/:marketType', marketsController.list);
 router.get('/:marketType/:asset', marketsController.detail);
-router.post('/:marketType/:asset/trade-confirmation', marketsController.tradeConfirmation);
 router.post('/:marketType/search', marketsController.search);
 
 module.exports = router;
