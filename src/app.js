f3b7e8cb588773c68c8de54b3e23c65d754d33a2
fjloromeyer <fjloromeyer@gmail.com>
2021-11-29 00:55:33 -0300

    creacion de pag error404 y puesta a punto de mothod-override para usar PUT y DELETE en app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const publicPath = path.join(__dirname, "../public");
 const mainRouter = require("./routers/mainRouter.js");
 const marketsRouter = require("./routers/marketsRouter.js");
 
+const methodOverride = require("method-override");
+
 // seteamos ruta de archivos estaticos y view engine por defecto
 app.use(express.static(publicPath));
 app.set("view engine", "ejs");
@@ -18,7 +20,13 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride("_method"));
+
 // rutas
 app.use("/", mainRouter);
 app.use("/markets", marketsRouter);
 
+//ruta que renderiza página de not-found cada vez que no encuentra una ruta
+app.use((req, res, next) => {
+  res.status(404).render("not-found");
+});
