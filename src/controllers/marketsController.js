68c9a7bf00d921dc3aaeebd4856b436f0d94f4a2
Darioque <darioskidelsky@gmail.com>
2021-12-07 14:16:31 -0300

    agrega links progresivos a edit y create, elimina estilos repetitivos y usa themes.css

--- src/controllers/marketsController.js
+++ src/controllers/marketsController.js
@@ -2,94 +2,95 @@
 const assetService = require("../services/assets");
 
 const marketsController = {
-  // funcion controladora para la pagina de "descubrir"/"mercados"
-  markets: function (req, res) {
-    const assetList = assetService.getAll();
-    res.render("products/markets", {
-      pageTitle: "UniFi - Markets",
-      // recibe lista ordenada y devuelve el primer activo (mayor ganador y mayor perdedor)
-      mainGainer: assetService.sortByGainers(assetList)[0],
-      mainLoser: assetService.sortByLosers(assetList)[0],
-      assetList,
-    });
-  },
+    // funcion controladora para la pagina de "descubrir"/"mercados"
+    markets: function (req, res) {
+        const assetList = assetService.getAll();
+        res.render("products/markets", {
+            pageTitle: "UniFi - Markets",
+            // recibe lista ordenada y devuelve el primer activo (mayor ganador y mayor perdedor)
+            mainGainer: assetService.sortByGainers(assetList)[0],
+            mainLoser: assetService.sortByLosers(assetList)[0],
+            assetList,
+        });
+    },
 
-  // funcion controladora para listar los activos de los mercados individuales
-  list: function (req, res) {
-    const marketType = req.params.marketType;
-    res.render("products/productList", {
-      marketType,
-      pageTitle: "Invest in UniFi - " + marketType,
-      assetList: assetService.getAssetList(marketType),
-      user: req.session.authenticatedUser,
-    });
-  },
+    // funcion controladora para listar los activos de los mercados individuales
+    list: function (req, res) {
+        const marketType = req.params.marketType;
+        res.render("products/productList", {
+            marketType,
+            pageTitle: "Invest in UniFi - " + marketType,
+            assetList: assetService.getAssetList(marketType),
+            user: req.session.authenticatedUser,
+        });
+    },
 
-  // funcion controladora para la pagina de detalle de cada activo
-  detail: function (req, res) {
-    const assetRequested = req.params.asset;
-    const marketType = req.params.marketType;
-    res.render("products/productDetail", {
-        asset: assetService.findAsset(marketType, assetRequested),
-        pageTitle:
-            assetService.findAsset(marketType, assetRequested).ticker +
-            " - Details",
-        user: req.session.authenticatedUser,
-    });
-  },
+    // funcion controladora para la pagina de detalle de cada activo
+    detail: function (req, res) {
+        const assetRequested = req.params.asset;
+        const marketType = req.params.marketType;
+        const asset = assetService.findAsset(marketType, assetRequested);
+        res.render("products/productDetail", {
+            asset,
+            pageTitle: asset.ticker + " - Details",
+            user: req.session.authenticatedUser,
+        });
+    },
 
-  // funcion controladora para el search bar del listado de activos que redirecciona al detalle
-  search: function (req, res) {
-    const marketType = req.params.marketType;
-    const assetRequested = req.body.search;
-    const asset = assetService.findAsset(marketType, assetRequested);
-    res.redirect("/markets/" + marketType + "/" + asset.id);
-  },
+    // funcion controladora para el search bar del listado de activos que redirecciona al detalle
+    search: function (req, res) {
+        const marketType = req.params.marketType;
+        const assetRequested = req.body.search;
+        const asset = assetService.findAsset(marketType, assetRequested);
+        res.redirect("/markets/" + marketType + "/" + asset.id);
+    },
 
-  // funcion controladora para el renderizado del formulario de creacion de activos
-  create: function (req, res) {
-    res.render("products/createProductForm", {
-      pageTitle: "Create Product - UniFi",
-    });
-  },
+    // funcion controladora para el renderizado del formulario de creacion de activos
+    create: function (req, res) {
+        const marketType = req.params.marketType;
+        res.render("products/createProductForm", {
+            pageTitle: "Create Product - UniFi",
+            marketType,
+        });
+    },
 
-  // funcion controladora para agregar el nuevo activo a la base de datos y mostrar nuevamente el listado
-  store: (req, res) => {
-    const asset = {
-      ...req.body,
-    };
+    // funcion controladora para agregar el nuevo activo a la base de datos y mostrar nuevamente el listado
+    store: (req, res) => {
+        const asset = {
+            ...req.body,
+        };
 
-    assetService.saveAssets(asset);
+        assetService.saveAssets(asset);
 
-    res.redirect("/markets/" + asset.type);
-  },
+        res.redirect("/markets/" + asset.type);
+    },
 
-  // funcion controladora para renderizar el formulario de edicion de activos
-  edit: function (req, res) {
-    const assetRequested = req.params.asset;
-    const marketType = req.params.marketType;
-    const asset = assetService.findAsset(marketType, assetRequested);
-    res.render("products/editProductForm", {
-      pageTitle: "Edit Product - UniFi",
-      asset,
-    });
-  },
+    // funcion controladora para renderizar el formulario de edicion de activos
+    edit: function (req, res) {
+        const assetRequested = req.params.asset;
+        const marketType = req.params.marketType;
+        const asset = assetService.findAsset(marketType, assetRequested);
+        res.render("products/editProductForm", {
+            pageTitle: "Edit Product - UniFi",
+            asset,
+        });
+    },
 
-  // funcion controladora para editar activos existentes en la base de datos
-  update: function (req, res) {
-    const assetId = req.params.asset;
-    const marketType = req.params.marketType;
-    assetService.updateAsset(req.body);
+    // funcion controladora para editar activos existentes en la base de datos
+    update: function (req, res) {
+        const assetId = req.params.asset;
+        const marketType = req.params.marketType;
+        assetService.updateAsset(req.body);
 
-    res.redirect(`/markets/${marketType}/${assetId}`);
-  },
+        res.redirect(`/markets/${marketType}/${assetId}`);
+    },
 
-  // funcion controladora para borrar activos existentes en la base de datos
-  delete: function (req, res) {
-    res.render("products/deleteProduct", {
-      pageTitle: "Delete Product - UniFi",
-    });
-  },
+    // funcion controladora para borrar activos existentes en la base de datos
+    delete: function (req, res) {
+        res.render("products/deleteProduct", {
+            pageTitle: "Delete Product - UniFi",
+        });
+    },
 };
 
 module.exports = marketsController;
