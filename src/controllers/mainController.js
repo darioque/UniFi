4cb827f37cbabad0c74d8ff0ff86b76e6fb93874
fjloromeyer <fjloromeyer@gmail.com>
2021-12-12 23:59:32 -0300

    creacion y validacion con multer y express validator de avatar en pagina registro

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,90 +3,91 @@ const assetService = require("../services/assets");
 const userService = require("../services/users");
 
 const mainController = {
-    index: function (req, res) {
-        res.render("home/index", {
-            pageTitle: "UniFi - Home",
-            cryptoList: assetService.getCrypto(),
-            stockList: assetService.getStock(),
-        });
-    },
-    login: function (req, res) {
-          res.render("users/login", {
-            pageTitle: "Log in",
-            old: req.session.rememberMe,
-          });
-    },
-    register: function (req, res) {
-        res.render("users/register", {
-            pageTitle: "Register",
-        });
-    },
-    processRegister: (req, res) => {
-        // guardamos los errores en una variable
-        const errors = validationResult(req);
-        // si hubo errores (la variable NO está vacía) mandarle los mensajes a la vista del formulario
-        if (!errors.isEmpty()) {
-            return res.render("users/register", {
-                pageTitle: "Register",
-                old: req.body,
-                errorMessages: errors.mapped(),
-            });
-        }
+  index: function (req, res) {
+    res.render("home/index", {
+      pageTitle: "UniFi - Home",
+      cryptoList: assetService.getCrypto(),
+      stockList: assetService.getStock(),
+    });
+  },
+  login: function (req, res) {
+    res.render("users/login", {
+      pageTitle: "Log in",
+      old: req.session.rememberMe,
+    });
+  },
+  register: function (req, res) {
+    res.render("users/register", {
+      pageTitle: "Register",
+    });
+  },
+  processRegister: (req, res) => {
+    // guardamos los errores en una variable
+    const errors = validationResult(req);
+    // si hubo errores (la variable NO está vacía) mandarle los mensajes a la vista del formulario
+    if (!errors.isEmpty()) {
+      return res.render("users/register", {
+        pageTitle: "Register",
+        old: req.body,
+        file: req.file, //no sé si agregarlo dentro del old para que lo traiga también si falla algo.
+        errorMessages: errors.mapped(),
+      });
+    }
 
-        // si no hubo errores en el formulario, intentar agregar el usuario a la base de datos
-        // si el email utilizado ya existia en la base de datos, retornar el sitio de registro con mensaje de error
-        // si no hay errores, redireccionar a login
-        if (!userService.addUser(req.body)) {
-            return res.render("users/register", {
-                pageTitle: "Register",
-                old: req.body,
-                errorMessages: [{ msg: "Email already registered" }],
-            });
-        } else {
-            res.redirect("/login");
-        }
-    },
-    // función para procesar autenticacion de usuarios
-    processLogin: function (req, res) {
-        const errors = validationResult(req);
-        // si hubo errores (la variable NO está vacía) mandarle los mensajes a la vista del formulario
-        if (!errors.isEmpty()) {
-            return res.render("users/login", {
-                errorMessages: errors.mapped(),
-                old: req.body,
-                pageTitle: "Login",
-            });
-        }
+    // si no hubo errores en el formulario, intentar agregar el usuario a la base de datos
+    // si el email utilizado ya existia en la base de datos, retornar el sitio de registro con mensaje de error
+    // si no hay errores, redireccionar a login
+    if (!userService.addUser(req.body)) {
+      return res.render("users/register", {
+        pageTitle: "Register",
+        old: req.body,
+        errorMessages: [{ msg: "Email already registered" }],
+      });
+    } else {
+      res.redirect("/login");
+    }
+  },
+  // función para procesar autenticacion de usuarios
+  processLogin: function (req, res) {
+    const errors = validationResult(req);
+    // si hubo errores (la variable NO está vacía) mandarle los mensajes a la vista del formulario
+    if (!errors.isEmpty()) {
+      return res.render("users/login", {
+        errorMessages: errors.mapped(),
+        old: req.body,
+        pageTitle: "Login",
+      });
+    }
 
-        // autentica al usuario y lo guarda en la variable user
-        const user = userService.authenticate(req.body);
+    // autentica al usuario y lo guarda en la variable user
+    const user = userService.authenticate(req.body);
 
-        // si no se encontró ningun usuario que coincida (credenciales invalidas), devolver el sitio de login con mensaje de error
-        if (!user) {
-            return res.render("users/login", {
-                errorMessages: [{ msg: "Invalid Credentials" }],
-            });
-        }
+    // si no se encontró ningun usuario que coincida (credenciales invalidas), devolver el sitio de login con mensaje de error
+    if (!user) {
+      return res.render("users/login", {
+        errorMessages: [{ msg: "Invalid Credentials" }],
+      });
+    }
 
-        // si no hubo errores, guardar al usuario autenticado con session y redirigir a home
-        req.session.authenticatedUser = user;
-        // si está tildado el campo de remember me, guardarlo con cookie
-        if (req.body.remember) {
-            res.cookie("rememberMe", user.id, { maxAge: 60000 });
-        }
-        
-        // si hay una url a redireccionar, llevarlo ahi al loguearse
-        if (req.session.redirectUrl) {
-            res.redirect(req.session.redirectUrl)
-        } else {
-            res.redirect("/");
-        }
-    },
-    // funcion para cerrar sesión
-    logout: function (req, res) {
-        req.session.destroy();
-        res.redirect("/");
-    },
+    // si no hubo errores, guardar al usuario autenticado con session y redirigir a home
+    req.session.authenticatedUser = user;
+    // si está tildado el campo de remember me, guardarlo con cookie
+    if (req.body.remember) {
+      res.cookie("rememberMe", user.id, { maxAge: 60000 });
+    }
+
+    // si hay una url a redireccionar, llevarlo ahi al loguearse
+    if (req.session.redirectUrl) {
+      res.redirect(req.session.redirectUrl);
+    } else {
+      res.redirect("/");
+    }
+  },
+  // funcion para cerrar sesión
+  logout: function (req, res) {
+    req.session.destroy();
+    res.redirect("/");
+  },
 };
 
 module.exports = mainController;
--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../public/img/avatars");
+  },
+  filename: (req, file, cb) => {
+    let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+    cb(null, fileName);
+  },
+});
+const uploadFile = multer({ storage });
+
 const { body } = require("express-validator");
 const mainController = require("../controllers/mainController");
 const logDBMiddleware = require("../middlewares/logDBMiddleware");
@@ -9,47 +23,60 @@ const rememberMeMiddleware = require("../middlewares/rememberMeMiddleware");
 
 // validaciones
 const registerValidations = [
-    body("email")
-        .notEmpty()
-        .withMessage("You need to set an email")
-        .bail()
-        .isEmail()
-        .withMessage("Invalid Email"),
-    body("password")
-        .notEmpty()
-        .withMessage("You need to set a password")
-        .bail()
-        .isLength({ min: 6, max: 15 })
-        .withMessage(
-            "Invalid Password (minimum length is 6 and max length is 15"
-        ),
+  body("email")
+    .notEmpty()
+    .withMessage("You need to set an email")
+    .bail()
+    .isEmail()
+    .withMessage("Invalid Email"),
+  body("password")
+    .notEmpty()
+    .withMessage("You need to set a password")
+    .bail()
+    .isLength({ min: 6, max: 15 })
+    .withMessage("Invalid Password (minimum length is 6 and max length is 15"),
+  body("avatar").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png"];
+    if (!file) {
+      throw new Error("You need to upload an image");
+    } else {
+      let fileExtension = path.extname(file.originalname); //no sé por que no me está tomando el file.originalname!
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `The accepted extensions are ${acceptedExtensions.join(", ")}`
+        );
+      }
+    }
+  }),
 ];
 
 const loginValidations = [
-    body("email").notEmpty().withMessage("You need to enter an email").bail(),
-    body("password").notEmpty().withMessage("You need to enter a password"),
+  body("email").notEmpty().withMessage("You need to enter an email").bail(),
+  body("password").notEmpty().withMessage("You need to enter a password"),
 ];
 
 router.get("/", mainController.index);
 router.get(
-    "/login",
-    guestMiddleware,
-    rememberMeMiddleware,
-    mainController.login
+  "/login",
+  guestMiddleware,
+  rememberMeMiddleware,
+  mainController.login
 );
 router.get("/register", guestMiddleware, mainController.register);
 router.get("/logout", mainController.logout);
 // con middleware de ruta
 router.post(
-    "/register",
-    logDBMiddleware,
-    registerValidations,
-    mainController.processRegister
+  "/register",
+  uploadFile.single("avatar"),
+  logDBMiddleware,
+  registerValidations,
+  mainController.processRegister
 );
 router.post(
-    "/login",
-    logDBMiddleware,
-    loginValidations,
-    mainController.processLogin
+  "/login",
+  logDBMiddleware,
+  loginValidations,
+  mainController.processLogin
 );
 module.exports = router;
