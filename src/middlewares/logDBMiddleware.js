6efd1c7ee2c19d8369ad29e5790d2e5dc8fffbf3
Darioque <darioskidelsky@gmail.com>
2021-12-21 00:04:23 -0300

    fix header y tamaño de imagenes

--- src/middlewares/logDBMiddleware.js
+++
@@ -1,7 +0,0 @@
-// middleware nivel ruta
-const fs = require("fs");
-function logDBMiddleware(req, res, next) {
-    fs.appendFileSync("logDB.txt", "\nSe creó un registro al ingresar en " + req.url);
-    next();
-}
-module.exports = logDBMiddleware;
--- src/middlewares/logMiddleware.js
+++
@@ -1,7 +0,0 @@
-// middleware global
-const fs = require('fs')
-function logMiddleware(req, res, next) {
-    fs.appendFileSync("log.txt", "\nSe ingreso en la pagina " + req.url);
-    next();
-}
-module.exports = logMiddleware;
