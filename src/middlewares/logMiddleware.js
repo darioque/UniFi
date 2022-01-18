665e3ede21d046a91532ad5d2692894a574bbc30
lucas <lucass.introzzi@gmail.com>
2022-01-18 16:02:19 -0300

    a

--- src/middlewares/logMiddleware.js
+++ src/middlewares/logMiddleware.js
@@ -1,7 +1,8 @@
 // middleware global
 const fs = require('fs')
 function logMiddleware(req, res, next) {
-    fs.appendFileSync("log.txt", "\nSe ingreso en la pagina " + req.url);
+    fs.appendFileSync('log.txt', 'Se ingreso en la pagina ' + req.url);
     next();
 }
 module.exports = logMiddleware;
+
