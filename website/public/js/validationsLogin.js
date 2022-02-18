9748af2495e76f3b89ea365873cb20c0fcf1d419
lucas <lucass.introzzi@gmail.com>
2022-02-18 19:02:47 -0300

    Validaciones front-end Login

---
+++ website/public/js/validationsLogin.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function() {
+
+
+const formulario = document.querySelector('form.login-form')
+const email = document.querySelector('input.email')
+const password = document.querySelector('input.password')
+const button = document.querySelector('button')
+
+    formulario.addEventListener('submit', function(e){
+        
+
+        let errores = []
+
+        if(email.value == "" ){
+            errores.push("You must enter an email")
+        }
+
+        if(password.value.length < 8 ){
+            errores.push("The password must have at least 8 caracters")
+        }
+
+        if(errores.length > 0){
+            e.preventDefault()
+
+            let ulErrores = document.querySelector("div.errores ul")
+
+            for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+            }
+        }
+    })
+
+    });
