2f3a39b9441ab442b3511148e83182366c03ef98
Dario Skidelsky <76440563+darioque@users.noreply.github.com>
2022-03-10 17:21:03 -0300

    change http to https

--- website/public/js/productDetail.js
+++ website/public/js/productDetail.js
@@ -113,7 +113,7 @@ window.onload = () => {
                 .split("/markets/")[1]
                 .split("/")[0];
             const response = await fetch(
-                `http://unifi-grupo6.herokuapp.com/api/${marketType}/markets/`
+                `https://unifi-grupo6.herokuapp.com/api/${marketType}/markets/`
             ).then((assets) => assets.json());
             const assets = response.data;
             const assetList = document.querySelector("#assetList");
